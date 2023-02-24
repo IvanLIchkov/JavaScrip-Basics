b87d1f82162f7e920fac53a5840fbb56598ba659
Scopi1 <Vanko.lichkov@abv.bg>
2023-02-24 10:08:42 +0200

    Udpate

---
+++ 10.ObjectAndClasses/object-syntax.js
@@ -0,0 +1,27 @@
+let personalInformation = {
+  firstName: "Ivan",
+  lastName: "Petrov",
+  age: 28,
+  eyeColor: 'Brown',
+  isMale: true
+};
+
+//Dynamic add property
+personalInformation.weight = 80;
+
+//delete property 
+delete personalInformation.weight;
+
+//using bracket syntax
+console.log(personalInformation['firstName']); //bracket syntax
+console.log(personalInformation.firstName); //dot syntax
+
+//acces property by variable
+let propertyName = 'firstName';
+console.log(personalInformation[propertyName]);
+
+//using different name for property name
+let carInfo = { //ако имаме ключ който е със специални символи и е нужно да е стринд той може да бъде достъпен само с carInfo['car-model'] бракет синтаксиса
+  'car-model': 'toyota',
+};
+console.log(carInfo['car-model']);  
